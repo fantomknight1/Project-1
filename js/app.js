9dc1ffe1f1b5efe6c5d6c347f16a6da7860cb260
Viorel Belfiore <belfiore.viorel@gmail.com>
2016-02-04 10:40:09 -0500

    project day 2

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  var $char1 = $('#character1');
+  $(document).keydown( function(e) {
+    switch(e.which) {
+      case 39:
+        console.log(e);
+        $('#character1').animate({right: "+=50px"});
+        break;
+    };
+  })
+})
+
+// $("body").keydown(function(e) {
+//   if(e.keyCode == 37) { // left
+//     $("#showroom").animate({
+//       left: "-=980"
+//     });
+
+// $(document).on('keydown', function moveUp(event) {
+//  if (87 === event.keyCode) {
+//    $ship.animate({'top':'-=4px'},0.05);
+//  } else if (event.keyCode === 83) {
+//    $ship.animate({'top':'+=4px'},0.05);
+//  }
+// })
