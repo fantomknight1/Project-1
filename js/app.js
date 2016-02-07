c590f1ba75f1f17cd53ed65421c5e3288006df16
Viorel Belfiore <belfiore.viorel@gmail.com>
2016-02-07 13:23:20 -0500

    made the enemies appear properly and move properly

--- js/app.js
+++ js/app.js
@@ -10,9 +10,39 @@ var obstacles;
 var $char1 = $('#character1');
 var $char2 = $('#character2');
 
+var object = 
+[{name: 'Jerry', width: '10px', height: '30px', position: 'absolute', left: '100%', bottom: '0px', background: 'green'},
+{name: 'Tammy', width: '9px', height: '25px', position: 'absolute', left: '100%', bottom: '0px', background: 'yellow'},
+{name: 'Mr. Meeseeks', width: '10px', height: '35px', position: 'absolute', left: '100%', bottom: '0px', background: 'blue'},
+{name: 'Mr. Meeseeks', width: '10px', height: '35px', position: 'absolute', left: '100%', bottom: '0px', background: 'blue'},
+{name: 'Mr. Meeseeks', width: '10px', height: '35px', position: 'absolute', left: '100%', bottom: '0px', background: 'blue'},
+{name: 'Mr. Meeseeks', width: '10px', height: '35px', position: 'absolute', left: '100%', bottom: '0px', background: 'blue'},
+{name: 'Mr. Meeseeks', width: '10px', height: '35px', position: 'absolute', left: '100%', bottom: '0px', background: 'blue'},
+{name: 'Telepathic Spider', width: '40px', height: '15px', position: 'absolute', left: '100%', bottom: '0px', background: 'black'},
+{name: 'Telepathic Spider', width: '40px', height: '15px', position: 'absolute', left: '100%', bottom: '0px', background: 'black'},
+{name: 'Telepathic Spider', width: '40px', height: '15px', position: 'absolute', left: '100%', bottom: '0px', background: 'black'},
+{name: 'Telepathic Spider', width: '40px', height: '15px', position: 'absolute', left: '100%', bottom: '0px', background: 'black'},
+{name: 'Counciler Rick 1', width: '10px', height: '30px', position: 'absolute', left: '100%', bottom: '0px', background: 'white'},
+{name: 'Counciler Rick 2', width: '10px', height: '30px', position: 'absolute', left: '100%', bottom: '0px', background: 'white'},
+{name: 'Counciler Rick 3', width: '10px', height: '30px', position: 'absolute', left: '100%', bottom: '0px', background: 'white'},
+{name: 'Counciler Rick 4', width: '10px', height: '30px', position: 'absolute', left: '100%', bottom: '0px', background: 'white'},
+{name: 'Federation Police', width: '12px', height: '25px', position: 'absolute', left: '100%', bottom: '0px', background: 'green'},
+{name: 'Federation Police', width: '12px', height: '25px', position: 'absolute', left: '100%', bottom: '0px', background: 'green'},
+{name: 'Federation Police', width: '12px', height: '25px', position: 'absolute', left: '100%', bottom: '0px', background: 'green'},
+{name: 'Federation Police', width: '12px', height: '25px', position: 'absolute', left: '100%', bottom: '0px', background: 'green'},
+{name: 'Federation Police', width: '12px', height: '25px', position: 'absolute', left: '100%', bottom: '0px', background: 'green'},
+{name: 'Snowball', width: '15px', height: '45px', position: 'absolute', left: '100%', bottom: '0px', background: 'green'},
+{name: 'Jellybean King', width: '15px', height: '25px', position: 'absolute', left: '100%', bottom: '0px', background: 'green'},
+{name: 'Abradolf Linkler', width: '10px', height: '30px', position: 'absolute', left: '100%', bottom: '0px', background: 'green'},
+{name: 'Cousin Nicki', width: '10px', height: '30px', position: 'absolute', left: '100%', bottom: '0px', background: 'green'},
+{name: 'Fart', width: '15px', height: '15px', position: 'absolute', left: '100%', bottom: '0px', background: 'green'},
+{name: 'Fart', width: '10px', height: '30px', position: 'absolute', left: '100%', bottom: '0px', background: 'green'},
+{name: 'Fart', width: '10px', height: '30px', position: 'absolute', left: '100%', bottom: '0px', background: 'green'},
+{name: 'Fart', width: '10px', height: '30px', position: 'absolute', left: '100%', bottom: '0px', background: 'green'}, 
+{name: 'Fart', width: '10px', height: '30px', position: 'absolute', left: '100%', bottom: '0px', background: 'green'}];
 
 
-obstacles
+
 
 
 
@@ -27,7 +57,7 @@ keys = [false, false, false, false, false, false];
 
 $(document).ready(function(){
   console.log('hi')
-
+  obstacles
   // obstacles = new component(10, 200, "green", 300, 120);
 
   $(document).on('keydown', function(ex){
@@ -81,16 +111,6 @@ $(document).ready(function(){
 
   })
 
-// //     .on('keyup', function(e) {
-// //        switch(e.which){
-// //         case 38:
-// //           console.log('let go of up key');
-// //           fall($char1)
-// //         break;
-// //        }
-// //     })
-//  });
-
   function checkKeys(e) {
   if(keys[1] || keys[0] || keys[2] || keys[3] || keys[4] || keys[5]) {
               
@@ -126,22 +146,54 @@ $(document).ready(function(){
   if ($char1.bottom > '150px') {
     $char1.bottom = '150px';
   }
+
+
+
+  function Enemy (name, width, height, background, position, left, bottom, move) {
+    this.name = name;
+    this.width = width;
+    this.height = height;
+    this.background = background;
+    this.position = position;
+    this.left = left;
+    this.bottom = bottom;
+    this.move = move;
+  }
+
+  Enemy.prototype.render = function() {
+
+    $('#upperSky').append($('<div>').addClass('enemyObject')
+      .css('width', this.width)
+      .css('height', this.height).css('background', this.background).css('position', this.position).css('left', this.left).css('bottom', this.bottom));
+  };
+
+  var enemyArray = [];
+
+  for (var i = 0; i < object.length; i++) {
+    var r = makeRandomNum(object.length);
+    var myObject = new Enemy(object[r].name, object[r].width, object[r].height, object[r].background, object[r].position, object[r].left, object[r].bottom);
+    enemyArray.push(myObject);
+  };
+  var intervalId;
+  var paintEnemies = function(){
+    if(!enemyArray.length) {
+      clearInterval(intervalId)
+    }
+
+    enemyArray.pop().render();
+  };
+  intervalId = setInterval(paintEnemies, 2000)
+  walkInterval = setInterval(moveObjectLeft, 1000)
+
+  function makeRandomNum(max) {
+    return Math.floor(Math.random() * max)
+  }
+
+  function moveObjectLeft() {
+    $('.enemyObject').finish().animate({left: '-=50px'})
+  }
+
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
